b14fdd1d1995ab835d5b25826750df4db3872670
Brent Hoskins <brenthoskins84@gmail.com>
2021-06-24 13:48:29 -0700

    color updates

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -33,7 +33,7 @@ class Experience extends Component {
             className="vertical-timeline-element--work"
             date={work.years}
             iconStyle={{
-              background: "#AE944F",
+              background: "#2B7A78",
               color: "#fff",
               textAlign: "center",
               display: 'flex',
@@ -81,7 +81,7 @@ class Experience extends Component {
             {work}
             <VerticalTimelineElement
               iconStyle={{
-                background: "#AE944F",
+                background: "#2B7A78",
                 color: "#fff",
                 textAlign: "center",
               }}
